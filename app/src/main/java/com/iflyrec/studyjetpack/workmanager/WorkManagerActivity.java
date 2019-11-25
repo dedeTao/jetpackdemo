37d3df5796705fb2eecaaa9d738fd28ecf5bc207
wqtao <wqtao@iflytek.com>
2019-11-25 17:33:10 +0800

    创建work

---
+++ app/src/main/java/com/iflyrec/studyjetpack/workmanager/WorkManagerActivity.java
@@ -0,0 +1,21 @@
+package com.iflyrec.studyjetpack.workmanager;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.iflyrec.studyjetpack.R;
+
+/**
+ * author : twq
+ * date : 2019/11/25
+ * desc :
+ */
+public class WorkManagerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workmanager);
+    }
+}
